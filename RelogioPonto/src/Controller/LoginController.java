5144dd2b02addbcb0fd0f516c9f3b4e107aa31bf
EMassashi <massashi21@gmail.com>
2020-10-19 18:56:13 -0300

    login and menu views

---
+++ RelogioPonto/src/Controller/LoginController.java
@@ -0,0 +1,49 @@
+package Controller;
+
+import View.Login;
+import View.MenuAdmin;
+import View.MenuUser;
+import java.awt.Color;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import util.ConectaBanco;
+import java.sql.SQLException;
+
+public class LoginController {
+    ConectaBanco conecta = new ConectaBanco();
+
+    private final Login view;
+
+    public LoginController(Login view) {
+        this.view = view;
+    }
+
+    public void login(JComboBox combo_user, JPasswordField txt_pass, JLabel message_txt) {
+        conecta.conexao();
+        try {
+            conecta.executaSQL("SELECT * FROM login WHERE username='" + combo_user.getSelectedItem() + "'");
+            conecta.rs.first();
+            if (conecta.rs.getString("password").equals(txt_pass.getText())) {
+                if(conecta.rs.getInt("isadmin") == 0){
+                    new MenuUser().setVisible(true);
+                    System.out.println("logou como usuario");
+                    view.dispose();
+                }else {
+                    if(conecta.rs.getInt("isadmin") == 1) {
+                        new MenuAdmin().setVisible(true);
+                        System.out.println("logou como admin");
+                        view.dispose();
+                    }
+                }
+            }else{
+                message_txt.setText("Senha inválida!");
+                message_txt.setForeground(new Color(202, 66, 66));
+            }
+        } catch (NullPointerException | SQLException ex) {
+            message_txt.setText("Usuário não existe!");
+            message_txt.setForeground(new Color(202, 66, 66));
+        }
+        conecta.desconecta();
+    }
+}
--- RelogioPonto/src/View/Login.java
+++ RelogioPonto/src/View/Login.java
@@ -1,22 +1,23 @@
-package main;
+package View;
 
-import util.ConectaBanco;
-import java.awt.Color;
+import Controller.LoginController;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
-import java.sql.SQLException;
 import util.FillCombo;
 
 public class Login extends javax.swing.JFrame {
 
-    ConectaBanco conecta = new ConectaBanco();
+    private final LoginController controller;
 
     public Login() {
         initComponents();
         setIcon();
         new FillCombo().preencherCombo("SELECT * FROM login ORDER BY username", combo_user, "username");
+        controller = new LoginController(this);
     }
 
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         jPanel2 = new javax.swing.JPanel();
@@ -24,11 +25,8 @@ public class Login extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         txt_pass = new javax.swing.JPasswordField();
-        jButton1 = new javax.swing.JButton();
         jLabel5 = new javax.swing.JLabel();
         jSeparator2 = new javax.swing.JSeparator();
-        jButton2 = new javax.swing.JButton();
-        jCheckBox1 = new javax.swing.JCheckBox();
         jButton3 = new javax.swing.JButton();
         message_txt = new javax.swing.JLabel();
         combo_user = new javax.swing.JComboBox();
@@ -36,7 +34,7 @@ public class Login extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Relógio Ponto - Login");
+        setTitle("Security System - Login");
         setUndecorated(true);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
@@ -55,11 +53,11 @@ public class Login extends javax.swing.JFrame {
         jPanel2.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(770, 0, 40, 30));
 
         jLabel1.setForeground(new java.awt.Color(91, 91, 91));
-        jLabel1.setText("password");
+        jLabel1.setText("Password");
         jPanel2.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 180, -1, -1));
 
         jLabel2.setForeground(new java.awt.Color(91, 91, 91));
-        jLabel2.setText("username");
+        jLabel2.setText("Username");
         jPanel2.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 110, -1, -1));
 
         txt_pass.setBorder(null);
@@ -70,20 +68,6 @@ public class Login extends javax.swing.JFrame {
         });
         jPanel2.add(txt_pass, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 200, 280, 30));
 
-        jButton1.setBackground(new java.awt.Color(255, 255, 255));
-        jButton1.setForeground(new java.awt.Color(150, 150, 150));
-        jButton1.setText("Sign Up");
-        jButton1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(162, 162, 162)));
-        jButton1.setContentAreaFilled(false);
-        jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        jButton1.setOpaque(true);
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-        jPanel2.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 330, 140, 30));
-
         jLabel5.setFont(new java.awt.Font("Segoe UI Black", 0, 24)); // NOI18N
         jLabel5.setForeground(new java.awt.Color(51, 60, 48));
         jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -92,18 +76,6 @@ public class Login extends javax.swing.JFrame {
 
         jSeparator2.setForeground(new java.awt.Color(195, 195, 195));
         jPanel2.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 230, 280, 10));
-
-        jButton2.setForeground(new java.awt.Color(154, 154, 154));
-        jButton2.setText("Forgot Password?");
-        jButton2.setContentAreaFilled(false);
-        jButton2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        jButton2.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
-        jPanel2.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 280, -1, -1));
-
-        jCheckBox1.setForeground(new java.awt.Color(154, 154, 154));
-        jCheckBox1.setText("Remember Me");
-        jCheckBox1.setOpaque(false);
-        jPanel2.add(jCheckBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 280, -1, -1));
 
         jButton3.setBackground(new java.awt.Color(39, 62, 174));
         jButton3.setForeground(new java.awt.Color(255, 255, 255));
@@ -120,7 +92,7 @@ public class Login extends javax.swing.JFrame {
 
         message_txt.setForeground(new java.awt.Color(135, 135, 135));
         message_txt.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        message_txt.setText("Bem vindo(a)!");
+        message_txt.setText("Welcome!");
         jPanel2.add(message_txt, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 240, 320, 20));
 
         combo_user.setEditable(true);
@@ -151,48 +123,44 @@ public class Login extends javax.swing.JFrame {
         jSeparator3.setForeground(new java.awt.Color(195, 195, 195));
         jPanel2.add(jSeparator3, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 160, 280, 10));
 
-        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/494641-PHESOO-113.jpg")));
-        jPanel2.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 0, 450, -1));
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/494641-PHESOO-113.jpg"))); // NOI18N
+        jPanel2.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 0, 450, -1));
 
         getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 820, 450));
 
         setSize(new java.awt.Dimension(814, 445));
         setLocationRelativeTo(null);
-    }
+    }// </editor-fold>//GEN-END:initComponents
 
-    private void txt_passKeyReleased(java.awt.event.KeyEvent evt) {
+    private void txt_passKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txt_passKeyReleased
         if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
-            login();
+            this.controller.login(combo_user, txt_pass, message_txt);
         }
-    }
+    }//GEN-LAST:event_txt_passKeyReleased
 
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {
-        login();
-    }
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        this.controller.login(combo_user, txt_pass, message_txt);
+    }//GEN-LAST:event_jButton3ActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
+    private void combo_userItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_combo_userItemStateChanged
+        //        camposenha.grabFocus();
+    }//GEN-LAST:event_combo_userItemStateChanged
 
-    }
+    private void combo_userFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_combo_userFocusLost
+        //
+    }//GEN-LAST:event_combo_userFocusLost
 
-    private void combo_userItemStateChanged(java.awt.event.ItemEvent evt) {
+    private void combo_userActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_combo_userActionPerformed
+        //
+    }//GEN-LAST:event_combo_userActionPerformed
 
-    }
+    private void combo_userKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_combo_userKeyPressed
+        //
+    }//GEN-LAST:event_combo_userKeyPressed
 
-    private void combo_userFocusLost(java.awt.event.FocusEvent evt) {
-
-    }
-
-    private void combo_userActionPerformed(java.awt.event.ActionEvent evt) {
-
-    }
-
-    private void combo_userKeyPressed(java.awt.event.KeyEvent evt) {
-
-    }
-
-    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         System.exit(0);
-    }
+    }//GEN-LAST:event_jButton4ActionPerformed
 
     public static void main(String args[]) {
         try {
@@ -211,7 +179,6 @@ public class Login extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Login().setVisible(true);
@@ -219,12 +186,10 @@ public class Login extends javax.swing.JFrame {
         });
     }
 
+    // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JComboBox combo_user;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
-    private javax.swing.JCheckBox jCheckBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel4;
@@ -234,25 +199,7 @@ public class Login extends javax.swing.JFrame {
     private javax.swing.JSeparator jSeparator3;
     private javax.swing.JLabel message_txt;
     private javax.swing.JPasswordField txt_pass;
-
-    public void login() {
-        conecta.conexao();
-        try {
-            conecta.executaSQL("SELECT * FROM login WHERE username='" + combo_user.getSelectedItem() + "'");
-            conecta.rs.first();
-            if (conecta.rs.getString("password").equals(txt_pass.getText())) {
-                //new Menu((String) combo_user.getSelectedItem()).setVisible(true); IMPLEMENTAR CLASSE MENU
-                dispose();
-            } else {
-                message_txt.setText("Invalid Password!");
-                message_txt.setForeground(new Color(202, 66, 66));
-            }
-        } catch (NullPointerException | SQLException ex) {
-            message_txt.setText("User does not exists!");
-            message_txt.setForeground(new Color(202, 66, 66));
-        }
-        conecta.desconecta();
-    }
+    // End of variables declaration//GEN-END:variables
 
     private void setIcon() {
         setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/Images/icone.png")));
